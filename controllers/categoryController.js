3fdf73a212b14d72040d9d8849b17cbe3dc303f6
Morphius22 <peaceandhealth@protonmail.com>
2023-06-11 13:08:56 -0700

    created admin login

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -14,8 +14,56 @@ exports.category_create_get = asyncHandler(async (req, res, next) => {
   });
 });
 
+//get admin login page
+exports.admin_get = asyncHandler(async (req, res, next) => {
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+  res.render("admin_login", {
+    title: "create category",
+    category_list: allCategories,
+  });
+});
+
 //post request to create a new category
-exports.category_create_post = asyncHandler(async (req, res, next) => {});
+exports.category_create_post = [
+  body("title", "title must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    console.log("we hit the async function");
+
+    const category = new Category({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      //there are errors - re-render form
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+      console.log("we hite an error");
+      res.render("create_category", {
+        title: "Error in Form",
+        category_list: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      //no errors. save new category
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+      console.log(allCategories);
+
+      await category.save();
+      console.log("a new category should have been saved");
+      res.render("index", {
+        title: "New category created",
+        category_list: allCategories,
+      });
+    }
+  }),
+];
 
 //get request to delete a new category
 exports.category_delete_get = asyncHandler(async (req, res, next) => {});
