1ee267aee851f475ab47fe48fae4aec1a88c07ea
Morphius22 <peaceandhealth@protonmail.com>
2023-06-13 20:50:50 -0700

    updated index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var router = express.Router();
+const Category = require("../models/category");
+const Item = require("../models/item");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.redirect("/item");
+router.get("/", async function (req, res, next) {
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+  const allItems = await Item.find()
+    .sort({ name: 1 })
+    .populate("category")
+    .exec();
+
+  console.log(allItems);
+  console.log(allCategories);
+  console.log(allCategories[0].name);
+
+  if (allCategories !== undefined) {
+    res.render("index", {
+      title: "Home Page",
+      category_list: allCategories,
+      item_list: allItems,
+    });
+  }
 });
 
 module.exports = router;
